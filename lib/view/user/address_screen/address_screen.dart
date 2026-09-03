0e7a303543488df3d92eae5c3e013a6077f2e9f7
omar <omarismail3939@gmail.com>
2026-09-03 16:40:40 +0300

    update

--- lib/view/user/address_screen/address_screen.dart
+++ lib/view/user/address_screen/address_screen.dart
@@ -2,7 +2,6 @@ import 'package:amazon/controller/services/user_data_crud_services/user_data_CRU
 import 'package:amazon/model/address_model.dart';
 import 'package:amazon/view/user/address_screen/widget/address_screen_text_field.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:uuid/uuid.dart';
 
 import '../../../constants/common_functions.dart';
